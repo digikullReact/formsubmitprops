98562caef93a28ad2fce9747d566a8ec5501f6a4
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-01-21 11:16:19 +0530

    updatres

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -6,8 +6,10 @@ const Api = () => {
 useEffect(function(){
 
     fetch('https://newsapi.org/v2/everything?q=apple&from=2021-12-18&to=2021-12-18&sortBy=popularity&apiKey=d0c699b779ac4b3097a0da9ed8caae13')
-  .then(response => response.json())
-  .then(json => setState(json.articles))
+  .then(response => {
+   return  response.json()
+  })
+  .then(json => console.log(json))
 
 // api calls ------>  url that provides you data
  
--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -1,19 +1,51 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
+
+
+import axios from 'axios';
+
+
 
 const Tip = () => {
+
+
+
+   
+
     const percentage=10;
+    const [apiData,setApiData]=useState([])
     const [peeps,setPeeps]=useState([])
-    const [sum,setTotalSum]=useState(0)
+    const [sum,setTotalSum]=useState(1)
     const [state,setState]=useState({
         name:"",
         amount:"",
         type:"",
         finalTip:""
     })
+
+
+    useEffect(()=>{
+        debugger;
+
+        axios.get("http://localhost:8081/getData").then(data=>{
+        
+      
+            setApiData(data["data"])
+        })
+      
+    },[sum])
+
+    useEffect(()=>{
+console.log("component has updated ")
+      
+      
+    })
+
     const handleChange=(event)=>{
         setState({...state,[event.target.name]:event.target.value})
 
     }
+
+    
 
     const getTipamount=(service)=>{
 
@@ -26,6 +58,28 @@ const Tip = () => {
             default:
                 break;
         }
+
+    }
+
+    const deleteIt=(id)=>{
+
+        debugger;
+        axios.delete("http://localhost:8081/deleteData/"+id).then(data=>{
+            console.log(data["data"]);
+            setTotalSum(sum*7);
+      ///  let  data2=apiData.filter(ele=>ele._id!=id)
+            //setApiData(data2);
+/*
+            axios.get("http://localhost:8081/getData").then(data=>{
+        
+      
+                setApiData(data["data"])
+            })
+            */
+            
+       
+          // setPeeps(8888);  /// state change ---------> what it does ? ---->it does re redering
+        })
 
     }
 
@@ -56,20 +110,26 @@ const Tip = () => {
         
             
 {
-    peeps.map(ele=>(
-        <div>
+    apiData.map((ele,i)=>(
+        <li key={ele._id}> 
+                  <div>
 <h1>
       {ele.name}
   </h1>
   <h1>
-      {ele.amount}
+      {ele.gender}
   </h1>
 
   <h1>
-      {ele.finalTip}
+      {ele.company}
   </h1>
-
+<button onClick={()=>deleteIt(ele._id)}>
+    Delete
+</button>
 </div>
+
+        </li>
+  
 
     ))
 }
